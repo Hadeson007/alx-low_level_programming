1a2485622f74564ad6258122e39fe5df420c03ee
Hadeson007 <Xxxkhidice@gmail.com>
2022-09-12 17:00:47 -0700

    low level

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+
+*main- entry point
+
+*Description: Alphabets letter
+
+*Return: 0
+
+*/
+
+int main(void)
+
+{
+
+	char alphabet = 'a';
+
+
+
+	while (alphabet <= 'z')
+
+	{
+
+		putchar(alphabet);
+
+		alphabet++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+
+*main- entry point
+
+*Description: Alphabets
+
+*Return: always 0
+
+*/
+
+int main(void)
+
+{
+
+	char alph = 'a';
+
+
+
+	while (alph <= 'z')
+
+	{
+
+		if (alph == 'e')
+
+		{
+
+			alph++;
+
+		}
+
+		else if (alph == 'q')
+
+		{
+
+			alph++;
+
+		}
+
+		else
+
+		{
+
+			putchar(alph);
+
+			alph++;
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
