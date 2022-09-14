134cde444d3033846629e83d049d6d36f1cb82bc
Hadeson007 <Xxxkhidice@gmail.com>
2022-09-14 15:56:41 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,8 +13,7 @@ void times_table(void)
 		_putchar('0');
 
 		for (mult = 1; mult <= 9; mult++)
-		{
-			
+		{			
 			_putchar(',');
 			_putchar(' ');
 
@@ -24,9 +23,11 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-			}
-			_putchar('\n');
+
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
+	}
 }
 
 
