9ad53d2351da17994554bde7c8643ceb6b7dce8c
Hadeson007 <Xxxkhidice@gmail.com>
2022-09-14 15:03:39 -0700

    quater

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * prints_last_digit - prints the last digit of a number
+ * @n: integer n
+ * Return: Value of the last digit.
+ */
+
+int print_last_digit(int n);
+{
+	int x;
+
+	if (n < 0);
+		n = -n;
+	x = n % 10;
+	if (x < 0)
+		x = -x;
+	_putchar(x + '0');
+	return (x);
+}
+
