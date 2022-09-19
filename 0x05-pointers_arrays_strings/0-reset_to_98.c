5d12e6c03d8ccba690f9c805a6d592ed21a9cd5d
Hadeson007 <Xxxkhidice@gmail.com>
2022-09-19 14:26:41 -0700

    done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,16 @@
-#include "main.h"
+ /*
+  * File: 0-reset_to_98.c
+  * Auth: sam
+  */
+
+#include "main.h"
 
 /**
- *  reset_to_98 - reset vakue in address referenced by pointer.
- *  @n: input integer.
- *
- *  Return: 0
+ * reset_to_98 - Takes a pointer to an int and updates
+ *               the value it points to to 98.
+ * @n: The pointer to an int.
  */
 void reset_to_98(int *n)
 {
-	*n = 98.
+	*n = 98;
 }
